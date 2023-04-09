6eae3f4ca60e6bf1afa4a6e439f8dca574568018
Kranthi <Kranthigowthamkumar@gmail.com>
2023-04-09 22:50:46 +0530

    Day 5 completed

---
+++ Basics/Day6/Rectangle.js
@@ -0,0 +1,9 @@
+const Shape = require('./Shape');
+
+function Rectangle(x, y, height, width) {
+    Shape.call(this, x, y);
+    this.height = height;
+    this.width = width;
+}
+
+Rectangle.prototype = Object.create(Shape.prototype);
---
+++ Basics/Day6/linking_protypes.js
@@ -0,0 +1,8 @@
+const Shape = require('./Shape');
+
+function Circle(x, y, radius) {
+    Shape.call(this, x, y);
+    this.radius = radius;
+}
+
+Circle.prototype = Object.create(Shape.prototype);
